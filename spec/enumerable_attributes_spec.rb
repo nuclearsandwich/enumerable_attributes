575b0c534bc57dfd534ec04fd221beaae1217dd4
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-10-18 18:31:17 -0700

    Move class creation into before block.

--- spec/enumerable_attributes_spec.rb
+++ spec/enumerable_attributes_spec.rb
@@ -1,18 +1,17 @@
-require 'rubygems'
-require 'minitest/spec'
-require 'minitest/pride'
 require 'minitest/autorun'
-require 'lib/enumerable_attributes'
+
+require './lib/enumerable_attributes'
 
 
 describe EnumerableAttributes do
+  before do
+    class ContainsManyCollections
+      include EnumerableAttributes
+    end
+  end
   describe "including it as a module" do
     it "defines attr_enumerator class method" do
-      class SomeClass
-        include EnumerableAttributes
-      end
-
-      SomeClass.must_respond_to :attr_enumerator
+      ContainsManyCollections.must_respond_to :attr_enumerator
     end
   end
 end
