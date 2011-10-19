cede30b5815a401905bcb76dca2b93ee4fbfd35c
Steven! Ragnarök <steven@nuclearsandwich.com>
2011-10-18 18:24:23 -0700

    First failing test.

---
+++ spec/enumerable_attributes_spec.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'minitest/spec'
+require 'minitest/pride'
+require 'minitest/autorun'
+
+puts Dir.pwd
+require 'lib/enumerable_attributes'
+
+
+describe EnumerableAttributes do
+  describe "including it as a module" do
+    it "defines attr_enumerator class method" do
+      class SomeClass
+        include EnumerableAttributes
+      end
+
+      SomeClass.must_respond_to :attr_enumerator
+    end
+  end
+end
